da30f9a37bb653c7d2f3fe4242ebfb2879f088a7
woopqww111 <woopqww111@yahoo.com>
2017-10-10 23:02:15 +0800

    添加外观模式的应用GameFacade

---
+++ Assets/Scripts/GameFacade.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public    class GameFacade
+{
+    private bool mIsGameOver = false;
+
+    public bool isGameOver
+    {
+        get { return mIsGameOver; }
+    }
+        public void Init()
+        {
+            
+        }
+
+        public void Update()
+        {
+            
+        }
+
+        public void Release()
+        {
+            
+        }
+    }
+
--- Assets/Scripts/SceneState/BattleState.cs
+++ Assets/Scripts/SceneState/BattleState.cs
@@ -10,5 +10,26 @@ public class BattleState : ISceneState
     {
        
     }
+    //兵营，关卡，角色管理，行动力，成就系统
+    private GameFacade mFacade;
+
+    public override void StateStart()
+    {
+        mFacade.Init();
+    }
+
+    public override void StateEnd()
+    {
+        mFacade.Release();
+    }
+
+    public override void StateUpdate()
+    {
+        if (mFacade.isGameOver)
+        {
+            Controller.SetState(new MainMenuState(Controller));
+        }
+        mFacade.Update();
+    }
 }
 
