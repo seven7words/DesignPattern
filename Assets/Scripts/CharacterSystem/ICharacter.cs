116314fe2f6439499c79c0705a739cc7f798190e
woopqww111 <woopqww111@yahoo.com>
2017-10-11 14:59:28 +0800

    完善角色系统基本框架

---
+++ Assets/Scripts/CharacterSystem/ICharacter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+ public  abstract class ICharacter
+ {
+
+
+     protected ICharacterAttr mAttr;
+     protected GameObject mGameObject;
+     protected NavMeshAgent mNavMeshAgent;
+     protected AudioSource mAudio;
+     
+ }
+
