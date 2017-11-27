457a2b15dc63dca489da1857bc2e89bebfe1a619
woopqww111 <woopqww111@yahoo.com>
2017-11-27 18:49:04 +0800

    Change:增加一个单例管理类

---
+++ Assets/Scripts/Manager/MonoSingleton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour where T:MonoBehaviour {
+
+	private T m_Instance;
+	public T Instance{
+		get{
+			return m_Instance;
+		}
+	}
+	private void Awake(){
+		if(m_Instance==null){
+			m_Instance = this as T;
+		}else{
+			Destroy(gameObject);
+		}
+	}
+	private void OnDestroy(){
+		if(this==m_Instance){
+			m_Instance=null;
+		}
+	}
+}
+public abstract class Singleton<T> where T:class,new(){
+	protected Singleton(){}
+	private static T m_Instance;
+	public static T Instance{
+		get{
+			if(m_Instance == null){
+				m_Instance = new T();
+			}
+			return m_Instance;
+		}
+	}
+	public static void Release(){
+		m_Instance = null;
+	}
+} 
